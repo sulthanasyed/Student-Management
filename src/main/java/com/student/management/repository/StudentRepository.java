d33c9a5ac1f5391a4b721f3f2eaf90246f0f0d20
sulthanasyed <syednageenasulthana@gmail.com>
2025-04-25 14:50:55 +0530

    Added Repository Layer

---
+++ src/main/java/com/student/management/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+package com.student.management.repository;
+
+
+import com.student.management.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByEmail(String email);
+}
